bd5313f67d7f0e27a278f90f14d923c2f8f1dfe8
Giovanni Berti <dev.giovanniberti@gmail.com>
2025-06-29 23:03:46 +0200

    feat: add first draft on elastic querying

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from leclerc!")
+import asyncio
+from elasticsearch import AsyncElasticsearch
+
+client = AsyncElasticsearch("http://localhost:9201")
+
+async def main():
+    resp = await client.search(
+        index=".ds-traces*",
+        body={
+            "query": {
+                "query_string": {
+                    "query": 'trace.id: "74c3dfca1ea4aa65b0c73780313a33cc"',
+                }
+            },
+            "size": 20
+        },
+    )
+
+    print("Got {} hits:".format(resp["hits"]["total"]["value"]))
+    for hit in resp["hits"]["hits"]:
+        data = hit["_source"]
+        print(f"span id: {data['span']['id']} span duration: {data['span']['duration']['us']} us")
+
 
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
