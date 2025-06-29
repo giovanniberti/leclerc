2179e4425a414ea252907d3bbe33fdbf5ef93db1
Giovanni Berti <dev.giovanniberti@gmail.com>
2025-06-29 23:03:46 +0200

    refactor(query): make script more clear about missing hits due to size constraint

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from elasticsearch import AsyncElasticsearch
 client = AsyncElasticsearch("http://localhost:9201")
 
 async def main():
+    max_returned_hits = 200
     resp = await client.search(
         index=".ds-traces*",
         body={
@@ -12,11 +13,12 @@ async def main():
                     "query": 'trace.id: "74c3dfca1ea4aa65b0c73780313a33cc"',
                 }
             },
-            "size": 20
+            "size": max_returned_hits
         },
     )
 
-    print("Got {} hits:".format(resp["hits"]["total"]["value"]))
+    hits = resp['hits']['total']['value']
+    print(f"Got {min(hits, max_returned_hits)} (out of {hits}) hits:")
     for hit in resp["hits"]["hits"]:
         data = hit["_source"]
         print(f"span id: {data['span']['id']} span duration: {data['span']['duration']['us']} us")
