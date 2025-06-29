9938e9bb7b51abbdcbb20ebf9df21f513374c352
Giovanni Berti <dev.giovanniberti@gmail.com>
2025-06-29 23:03:47 +0200

    feat(query): add pagination with point-in-time API

--- main.py
+++ main.py
@@ -4,40 +4,72 @@ from elasticsearch import AsyncElasticsearch
 client = AsyncElasticsearch("http://localhost:9201")
 
 async def main():
-    max_returned_hits = 200
-    resp = await client.search(
-        index=".ds-traces*",
-        body={
-            "query": {
+    pit_keep_alive = "1m"
+    point_in_time = (await client.open_point_in_time(
+        keep_alive=pit_keep_alive,
+        allow_partial_search_results=False,
+        index=".ds-traces*"
+    ))["id"]
+
+    max_returned_hits = 10_000
+    search_after = None
+    hits = 1
+    i = 0
+    total_hits = 0
+    
+    while True:
+        resp = await client.search(
+            pit={"id": point_in_time, "keep_alive": pit_keep_alive},
+            allow_partial_search_results=False,
+            query={
                 "bool": {
                     "must": [
                         {
                             "range": {
                                 "@timestamp": {
-                                    "gt": "2025-04-17T09:40:39.809Z"
+                                    "gt": "2025-04-30T16:12:00.000+02:00",
+                                    "lt": "2025-04-30T16:30:00.000+02:00"
                                 }
                             }
                         },
                         {
                             "query_string": {
-                                "query": "trace.id: \"74c3dfca1ea4aa65b0c73780313a33cc\""
+                                "query": "transaction.name: \"POST /v1/rule/disable\""
                             }
                         }
                     ]
                 }
             },
-            "size": max_returned_hits
-        },
-    )
+            timeout="60s",
+            search_after=search_after,
+            size=max_returned_hits,
+            sort={
+                "@timestamp": {
+                    "order": "asc",
+                    "format": "strict_date_optional_time_nanos",
+                    "numeric_type": "date_nanos"
+                }
+            }
+        )
+        
+        i += 1
+        print(f"Search {i} done")
+        hits = len(resp["hits"]["hits"])
+        
+        if hits == 0:
+            break
 
-    hits = resp['hits']['total']['value']
-    print(f"Got {min(hits, max_returned_hits)} (out of {hits}) hits:")
-    for hit in resp["hits"]["hits"]:
-        data = hit["_source"]
-        if "transaction" in data:
-            print(f"transaction id: {data['transaction']['id']} name: \"{data['transaction']['name']}\" transaction duration: {data['transaction']['duration']['us']} us")
-        else:
-            print(f"span id: {data['span']['id']} name: \"{data['span']['name']}\" span duration: {data['span']['duration']['us']} us")
+        total_hits += hits
+        point_in_time = resp["pit_id"]
+        search_after = resp['hits']['hits'][-1]['sort']
+    
+        print(f"Got {min(hits, max_returned_hits)} (out of {hits}) hits:")
+        
+        if hits < max_returned_hits:
+            break
+
+    print(f"{total_hits=}")
+    await client.close_point_in_time(id=point_in_time)
 
 
 
